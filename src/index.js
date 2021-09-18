9aa83da59d7796b2c8167dfdc4e4ccc1bdd0f74d
ta-moli <yarnusha7@gmail.com>
2021-09-19 02:31:30 +0300

    changed homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './sass/main.scss';
+Есть файл fetchCountries.js с дефолтным экспортом функции fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
+
+var debounce = require('lodash.debounce');
