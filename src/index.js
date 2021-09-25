c7f2f01ac096b42705a73eb80f493bdd068c69b0
ta-moli <yarnusha7@gmail.com>
2021-09-26 00:05:07 +0300

    added last change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import './sass/main.scss';
-Есть файл fetchCountries.js с дефолтным экспортом функции fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
+import './js/countries.js'
 
-var debounce = require('lodash.debounce');
